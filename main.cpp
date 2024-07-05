c5bff2e7011352ecf34791564675e2ead3984e9a
hase-bTech383 <hs5163447@gmail.com>
2024-07-05 10:09:38 +0500

    Second commit don't know the change

--- main.cpp
+++ main.cpp
@@ -1,43 +1,47 @@
 #include <iostream>
 #include <string>
 
+std::string names[] = { "Honda", "Toyota", "Mercedes", "KIA", "BMW" };
+
 struct car {
 public:
     std::string name;
     int model;
     int engine;
+
+    car() {
+        name = "";
+        model = 0;
+        engine = 0;
+    }
 };
 
 void setCar(car* A) {
-    std::string names[] = {"Honda", "Toyota", "Mercedes", "KIA", "BMW"}; 
-    int models[5] = {2024,2022,2010,2020,2018};
-    int engines[5] ={1800, 1600, 2000, 1600, 1800} ;
+    int models[5] = { 2024,2022,2010,2020,2018 };
+    int engines[5] = { 1800, 1600, 2000, 1600, 1800 };
     for (int i = 0; i < 5; i++) {
-        (A + i)-> model = models[i];
+        (A + i)->model = models[i];
         (A + i)->engine = engines[i];
         (A + i)->name = names[i];
     }
 }
 
 void getCar(car* A, int n) {
-    for (int i = 0; i < sizeof(A)/sizeof(A[0]); i++) {
-        if(i+1 == n){
-            std::cout << "The attributes of Car " << i << " are: " << std::endl;
-            std::cout << ' ' << A->name << std::endl;
-            std::cout << ' ' << A->model << std::endl;
-            std::cout << ' ' << A->engine << std::endl;
-            std::cout << std::endl;
-        }
-        
-    }
-    
+    std::cout << "The attributes of Car " << n << " are: " << std::endl;
+    std::cout << ' ' << "Name:" << (A + n - 1)->name << std::endl;
+    std::cout << ' ' << "Model:" << (A + n - 1)->model << std::endl;
+    std::cout << ' ' << "Engine CC:" << (A + n - 1)->engine << std::endl;
+    std::cout << std::endl;
 }
 
 int main() {
-    car myCar[10];
+    car myCar[5];
     int n;
-    std::cout<< " Please Enter the car number: ";
-    std::cin>>n;
+    for (int i = 0; i < 5; i++) {
+        std::cout << i+1 <<". " << names[i] << std::endl;
+    }
+    std::cout << " Please Enter the car number: ";
+    std::cin >> n;
     setCar(myCar);
     getCar(myCar, n);
 }
